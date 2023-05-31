1b563bb7dcb525befd8ad5cee10846f8983fc885
Amal <amal.zguem@esprit.tn>
2023-05-31 09:27:14 +0100

    Creation entite Reclamation

---
+++ src/main/java/Controller/ReclamationController.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import Services.IPostService;
+import Services.IReclamationService;
+import com.gesconge.gesconge.Entities.Post;
+import com.gesconge.gesconge.Entities.Reclamation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Reclamation")
+public class ReclamationController {
+    @Autowired
+    private IReclamationService ReclamationService;
+
+    @GetMapping("/AllRec")
+    @ResponseBody
+    public List<Reclamation> retrieveAllReclamations() {
+        return ReclamationService.retrieveAllReclamations();
+    }
+
+    @GetMapping("/ReclamationID/{id}")
+    @ResponseBody
+    public Reclamation retrieveReclamation(@PathVariable("id") Long id) {
+        return ReclamationService.retrieveReclamation(id);
+    }
+
+    @PostMapping("/addRec")
+    @ResponseBody
+    public Reclamation addReclamation(Reclamation r) {
+        return ReclamationService.addReclamation(r);
+    }
+
+    @PutMapping("/update/Reclamation")
+    @ResponseBody
+    public Reclamation updateReclamation(Reclamation r) {
+        return ReclamationService.updateReclamation(r);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public void deleteReclamation(Long id) {
+        ReclamationService.deleteReclamation(id);
+    }
+
+}
---
+++ src/main/java/Services/ReclamationService.java
@@ -0,0 +1,43 @@
+package Services;
+
+import com.gesconge.gesconge.Entities.Reclamation;
+import com.gesconge.gesconge.Repositories.IReclamation;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class ReclamationService implements IReclamationService{
+    @Autowired
+    IReclamation r;
+    public Reclamation addReclamation(Reclamation reclamation){
+        return r.save(reclamation);
+    }
+    public void deleteReclamation(Long id){
+        r.deleteById(id);
+    }
+    public List<Reclamation> retrieveAllReclamations(){
+        return r.findAll();
+    }
+    public Reclamation updateReclamation(Reclamation updatedRec){
+        Reclamation reclamation = r.findById(updatedRec.getIdReclamation()).get();
+
+
+            // Update the properties of the existing client with the updated values
+            reclamation.setType(updatedRec.getType());
+            reclamation.setDateCreation(updatedRec.getDateCreation());
+            reclamation.setEtat(updatedRec.getEtat());
+
+            // Save the updated client and return the updated entity
+          return r.save(reclamation);
+
+    }
+    public Reclamation retrieveReclamation(Long id){
+        return r.getReferenceById(id);
+    }
+}
